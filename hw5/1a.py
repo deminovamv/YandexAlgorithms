b9015da291ae3cd0ff1da5569a36101e329e19ef
deminovamv <deminovamv@gmail.com>
2021-11-24 23:00:36 +0300

    decision passed

--- hw5/1a.py
+++ hw5/1a.py
@@ -10,7 +10,15 @@
 
 
 n, q = map(int, input().split())
-array_n = list(map(int, input().split()))
+array_n = [int(elem) for elem in input().split()]
+pref_sum = [0] * (n + 1)
+# for _ in range(q):                 #долго работает
+#    left, right = input().split()
+#    print(sum(array_n[int(left) - 1: int(right)]))
+
+for i in range(1, n + 1):
+    pref_sum[i] = pref_sum[i - 1] + array_n[i - 1]
+
 for _ in range(q):
-    left, right = map(int, input().split())
-    print(sum(array_n[left - 1 : right]))
+    left, right = input().split()
+    print(pref_sum[int(right)] - pref_sum[int(left) - 1])
