2819da45dba2509298a4ebd3e1a1b057fe5cefda
deminovamv <deminovamv@gmail.com>
2021-11-30 21:27:15 +0300

    solutions hw 6 task a

--- hw6/1a.py
+++ hw6/1a.py
@@ -2,7 +2,7 @@ n = int(input())
 nums = sorted([int(el) for el in input().split()])
 k = int(input())
 # print(nums)
-ans = ['0'] * (n - 1)
+ans = ['0'] * k
 
 def check(m, num):
     return nums[m] >= num
@@ -40,7 +40,7 @@ for i in range(k):
     right_i = right_binary_search(r)
     # print(left_i, right_i)
     if (left_i == right_i == 0 and nums[0] > r) or (left_i == right_i == (n - 1) and nums[n-1] < l):
-        ans[i] = 0
+        ans[i] = str(0)
     else:
         ans[i] = str(right_i - left_i + 1)
 print(" ".join(ans))
